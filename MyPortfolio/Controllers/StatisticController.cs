a06071c4adb0822c89e70d82b479bcf761ee8b10
Fahrettin Solak <fhrttnsolak96@gmail.com>
2024-07-16 19:04:14 +0300

    Update Code

---
+++ MyPortfolio/Controllers/StatisticController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+namespace MyPortfolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+		public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
